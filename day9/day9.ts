fc770fbf59371feeeff2c30cf960403b6dd2cb60
GGCristo <cgg.code@gmail.com>
2021-12-14 01:24:55 +0000

    Improve day9 part1

--- day9/day9.ts
+++ day9/day9.ts
@@ -1,8 +1,9 @@
 function checkLowPoint(heightmap: number[][], row: number, column :number): number {
-  if ((row - 1 >= 0 && heightmap[row - 1][column] <= heightmap[row][column]) ||
-      (row + 1 < heightmap.length && heightmap[row + 1][column] <= heightmap[row][column]) ||
-      (column - 1 >= 0 && heightmap[row][column - 1] <= heightmap[row][column]) ||
-      (column + 1 < heightmap[row].length && heightmap[row][column+1] <= heightmap[row][column])) {
+  const CHECKNORTH = () => row - 1 >= 0 && heightmap[row - 1][column] <= heightmap[row][column]
+  const CHECKSOUTH = () => row + 1 < heightmap.length && heightmap[row + 1][column] <= heightmap[row][column]
+  const CHECKWEST = () => column - 1 >= 0 && heightmap[row][column - 1] <= heightmap[row][column]
+  const CHECKEAST = () => column + 1 < heightmap[row].length && heightmap[row][column + 1] <= heightmap[row][column]
+  if (CHECKNORTH() || CHECKSOUTH() || CHECKWEST() || CHECKEAST()) {
     return 0
   }
   return 1 + heightmap[row][column]
